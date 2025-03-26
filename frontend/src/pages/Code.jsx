98a3d1a127d578eef1f8cd3a3dabf2f392f38c73
ChandraShekharSaini <chandrashekharsaini322@gmail.com>
2025-03-27 00:32:02 +0530

    added

--- frontend/src/pages/Code.jsx
+++ frontend/src/pages/Code.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Code = () => {
     return (
-        <section className='w-full h-screen text-white text-2xl flex items-center justify-center '>
-            <p>I am Code Page</p>
+        <section className='w-full h-screen bg-black flex items-center justify-center '>
+            <p className='text-white text-2xl'>I am Code Page</p>
         </section>
     )
 }
